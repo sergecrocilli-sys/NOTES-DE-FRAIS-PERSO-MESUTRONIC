3471f239f59d6ba90d2b487ad83aa0e42274c9d0
sergecrocilli-sys <serge.crocilli@gmail.com>
2026-08-10 16:00:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mesutronic-ndf-v6';
+const CACHE='mesutronic-ndf-v6-1';
 const CORE=['./','./index.html','./manifest.webmanifest','./modele-note-frais.xlsx','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
